14523b8e8d0e2615f109c289be28e17f543cc930
LWJ <40205003@im.wfu.edu.tw>
2020-05-06 23:08:38 +0800

    init vue.

---
+++ src/components/Home.vue
@@ -0,0 +1,73 @@
+<template>
+	<div id="wrap">
+		<article v-for="art in articles">
+			<header>
+				<h1>{{ art.title }}</h1>
+				<i>{{ art.date | fmDate }}</i>
+			</header>
+			<section>
+				<p>{{ art.content | fmContent }}</p>
+			</section>
+		</article>
+	</div>
+</template>
+
+<style lang="scss" scoped>
+	#wrap {
+		text-align: left;
+	}
+
+	article {
+		border-radius: 10px;
+		border: 2px solid gray;
+		margin: 20px 5px;
+		padding: 5px;
+
+		header h1 {
+			font-weight: bold;
+
+		}
+
+		header h1::before {
+			content: '＊';
+			font-size: 26px;
+		}
+		header i {
+			color: #999;
+		}
+		section p {
+			padding: 5px;
+			font-family: '標楷體';
+			font-size: 18px;
+		}
+	}
+</style>
+
+<script>
+	import axios from 'axios';
+
+	export default {
+		name: 'Home',
+		mounted() {
+			const api = 'https://us-central1-expressapi-8c039.cloudfunctions.net/app/article';
+			axios.get(api).then(result => {
+				this.articles = result.data.data;
+			});
+		},
+		data() {
+			return {
+				articles: null,
+			}
+		},
+
+		filters: {
+			fmContent: str => {
+				return str.length > 150 ? str.substring(0, 150) + '．．．' : str;				
+			},
+			
+			fmDate: str => {
+				return new Date(str);
+			}
+		}
+	}
+</script>
