34fe87a324cccec6895d6451a4e11d65fd3be1c1
LWJ <40205003@im.wfu.edu.tw>
2020-05-06 23:07:57 +0800

    init vue.

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,37 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <Header />
+
+    <router-view />
+
   </div>
 </template>
 
+<script>
+  // import HelloWorld from '@/components/HelloWorld.vue';
+  import Header from '@/components/Header.vue';
+
+  export default {
+    data() {
+      return {
+
+      }
+    },
+    components: {
+      Header
+    }
+  }
+
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
 
 #nav {
   padding: 30px;
