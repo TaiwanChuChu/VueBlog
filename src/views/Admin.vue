39781e36ccfc19bac1cedd104023cc9983a40972
LWJ <40205003@im.wfu.edu.tw>
2020-05-07 23:39:43 +0800

    update.

---
+++ src/views/Admin.vue
@@ -0,0 +1,6 @@
+<template>
+	<div>
+		<router-link to="/admin">admin-home</router-link>
+		<router-link to="/admin/add">admin-add</router-link>
+	</div>
+</template>
---
+++ src/views/Article.vue
@@ -0,0 +1,3 @@
+<template>
+	<h1>This is a Article page</h1>
+</template>
