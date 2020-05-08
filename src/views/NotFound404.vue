24ad8f633430d97fc8d53469664d3212275ad330
LWJ <40205003@im.wfu.edu.tw>
2020-05-08 23:44:13 +0800

    update

---
+++ src/views/NotFound404.vue
@@ -0,0 +1,17 @@
+<template>
+	<div>
+		<h1>404</h1>
+		<h4>This page is not found!</h4>
+		<b-button @click="routeToHome">Go Back!</b-button>
+	</div>
+</template>
+
+<script>
+	export default {
+		methods: {
+			routeToHome: function() {
+				this.$router.push({name: 'Home'});
+			}
+		}
+	}
+</script>
